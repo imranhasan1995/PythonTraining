1578ef0f13bd34bcf04c87557c144825c81073bb
Md. Imran Hasan <imranhasan@ad.enosisbd.com>
2026-01-07 17:16:48 +0600

    Topic-14: Practice getattr, setattr, hasattr, print functions

--- BuildInFunctions.py/buildInFunctions.py
+++ BuildInFunctions.py/buildInFunctions.py
@@ -1,20 +1,20 @@
-### range() function: range(start, stop, step)
+# range() function: range(start, stop, step)
 print("Even numbers between given range:")
 for index in range(12, 20, 2):
     print(index)
 
-### enumerate() function: enumerate(iterable, startIndex)
+# enumerate() function: enumerate(iterable, startIndex)
 
 fruitLst = ["Grapes", "Apple", "Banana", "Kiwi"]
 print("The newly created list:")
 for index, fruit in enumerate(fruitLst):
-   print(f"Index: {index}, Value: {fruit}")
+    print(f"Index: {index}, Value: {fruit}")
 
 fruits = [(8, "Grapes"), (10, "Apple"), (9, "Banana"), (12, "Kiwi")]
 for index, (quantity, fruit) in enumerate(fruits):
-   print(f"Index: {index}, Quantity: {quantity}, Fruit: {fruit}")
+    print(f"Index: {index}, Quantity: {quantity}, Fruit: {fruit}")
 
-## any and all function
+# any and all function
 """
 returns True if any of the elements of a given iterable, such as a list, tuple, 
 set, or dictionary is truthy, otherwise, it returns False.
@@ -31,24 +31,24 @@ numerics = [71, 87, 44, 34, 15, "", 5]
 output = all(numerics)
 print("The output after evaluation:", output)
 
-### sum() function
-x = [10,20,30]
+# sum() function
+x = [10, 20, 30]
 total = sum(x)
-print ("x: ",x, "sum(x): ", total)
+print("x: ", x, "sum(x): ", total)
 
 x = (10, -20, 10)
 total = sum(x)
-print ("x: ",x, "sum(x): ", total)
+print("x: ", x, "sum(x): ", total)
 
-### min() and max() function
+# min() and max() function
 # Creating a list
-List = [74,587,24,92,4,2,7,46]
+List = [74, 587, 24, 92, 4, 2, 7, 46]
 res = max(List)
 print("The largest number in the list is: ", res)
 res = min(List)
 print("The Smallest number in the list is: ", res)
 
-### sorted() function
+# sorted() function
 numericList = [88, 89, 81, 82, 86, 85, 83]
 print("Before Sorting:", numericList)
 print("Printing the list items after sorting:")
@@ -60,29 +60,37 @@ print("sorting the list items by length:")
 print(sorted(orgnlStrList, key=len, reverse=True))
 
 
-### reversed() function
+# reversed() function
 numericList = [12, 14, 18, 20, 10, 5]
 print("Displaying list in reverse order:")
 for revList in reversed(numericList):
-   print(revList)
+    print(revList)
 
-### isinstance() function
+# isinstance() function
+
+
 class Seasons:
-   pass
+    pass
+
 
 class Spring(Seasons):
-   pass
+    pass
+
 
 springObj = Spring()
 output = isinstance(springObj, Seasons)
 print("The given object is instance of Seasons class:", output)
 
-### issubclass() function
+# issubclass() function
+
+
 class ClassParent:
-   pass
+    pass
+
 
 class ClassChild(ClassParent):
-   pass
+    pass
+
 
 output = issubclass(ClassChild, ClassParent)
-print("Is ClassChild is child class of ClassParent:", output) 
+print("Is ClassChild is child class of ClassParent:", output)
---
+++ BuildInFunctions.py/buildInFunctions2.py
@@ -0,0 +1,23 @@
+# getattr(), setattr(), hasattr()
+
+info = ["name", "emp_id", "status"]
+value = ["Imran", 40, "Present"]
+
+
+class Emp:
+    def __init__(self, name, emp_id, status):
+        self.name = name
+        self.emp_id = emp_id
+        self.status = status
+
+
+emp = Emp("Ansh", 30, "Present")
+print("The information of Employee:")
+for i in range(len(info)):
+    print(getattr(emp, info[i]))
+    setattr(emp, info[i], value[i])
+    print("New Value:",getattr(emp, info[i]))
+    print(f"property {info[i]} status:{hasattr(emp, info[i])}")
+
+### print() function
+print("TutorialsPoint", "is", "located", "in", "India.", sep="-")
